b5968974d8a3c24f6d272720cafb874defaed752
Samandar <82985543+Manguberdi2002@users.noreply.github.com>
2023-02-05 10:43:53 +0500

    the best Loader

---
+++ src/UI/loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+   <div class="Loader">
+      <div className="lds-hourglass"></div>
+      <div className="fw-bold title">Loading...</div>
+   </div>
+  );
+};
+
+export default Loader;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Loader } from "../UI";
 
 const Main = () => {
-  const { articles } = useSelector((state) => state.article);
+  const { articles, isLoading } = useSelector((state) => state.article);
   return (
     <div className="container">
+      {isLoading && <Loader/>}
+      
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         {articles.map(item =>(
         <div className="col" key={item.id}>
@@ -23,9 +26,10 @@ const Main = () => {
               <rect width="100%" height="100%" fill="#55595c"></rect>
             </svg>
             <div className="card-body">
-              <p className="card-text fw-bold">{item.title}</p>
+              <p className="card-text fw-bold m-1">{item.title}</p>
               <p className="card-text">{item.description}</p>
-              <div className="d-flex justify-content-between align-items-center">
+            </div>
+              <div className="card-footer d-flex justify-content-between align-items-center">
                 <div className="btn-group">
                  
                   <button type="button" className="btn btn-sm btn-outline-primary">View</button>
@@ -34,7 +38,6 @@ const Main = () => {
                 </div>
                 <small className="text-muted fw-bold">{item.author.username}</small>
               </div>
-            </div>
           </div>
         </div>
         ))}
